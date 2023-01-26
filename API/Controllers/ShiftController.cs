e68debda808a06ce73cafedf68db9c95d692dd6d
waleed <waleed.sukhon77@gmail.com>
2023-01-27 00:58:58 +0200

    all notnet api are done

--- API/Controllers/ShiftController.cs
+++ API/Controllers/ShiftController.cs
@@ -19,5 +19,40 @@ namespace API.Controllers
         {
             return await context.EmployeeShifts.Include(e => e.Employee).Include(e => e.Shift).ToListAsync();
         }
+        [HttpPut()]
+        public async Task<int> update([FromBody] Shift dot)
+        {
+            return await context.Shifts.Where(a => a.Id == dot.Id).ExecuteUpdateAsync(b => b.SetProperty(a => a.StartTime, dot.StartTime)
+            .SetProperty(a => a.EndTime, dot.EndTime)
+            .SetProperty(a => a.Date, dot.Date));
+        }
+        [HttpPost()]
+        public async Task<bool> add([FromBody] Shift dot)
+        {
+            try
+            {
+                await context.Shifts.AddAsync(dot);
+                await context.SaveChangesAsync();
+            }
+            catch
+            {
+                return false;
+
+            }
+            return true;
+        }
+        [HttpDelete()]
+        public async Task<bool> delete([FromBody] Shift dot)
+        {
+            var department = await context.Shifts.SingleOrDefaultAsync(item => item.Id == dot.Id);
+
+            if (department != null)
+            {
+                context.Shifts.Remove(department);
+                await context.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
     }
 }
